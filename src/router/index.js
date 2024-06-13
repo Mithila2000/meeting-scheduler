bd661d37f44e939661ede66b58699e6da8358cd8
Mithila2000 <mithilasonar17@gmail.com>
2024-06-13 23:54:14 +0530

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AdminView from '../views/AdminView.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: HomeView
+    },
+    {
+        path: '/admin',
+        name: 'Admin',
+        component: AdminView,
+        meta: { requiresAdmin: true }
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAdmin)) {
+        const isAdmin = true
+        if (!isAdmin) {
+            next('/')
+        } else {
+            next()
+        }
+    } else {
+        next()
+    }
+})
+
+export default router
